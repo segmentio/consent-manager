f4bdf2a645c04048cbb43f7a21f184cd21a7016e
Netto Farah <nettofarah@gmail.com>
2019-10-04 15:54:53 -0700

    Ignore build

--- build/consent-manager/font-styles.js
+++
@@ -1,8 +0,0 @@
-var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
-    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
-    return cooked;
-};
-import { css } from 'react-emotion';
-export default css(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,\n    Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',\n    'Segoe UI Symbol';\n  -moz-osx-font-smoothing: grayscale;\n  -webkit-font-smoothing: antialiased;\n  font-smoothing: antialiased;\n  color: #435a6f;\n  font-size: 16px;\n  font-weight: 400;\n  line-height: 22px;\n  letter-spacing: -0.05px;\n"], ["\n  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,\n    Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',\n    'Segoe UI Symbol';\n  -moz-osx-font-smoothing: grayscale;\n  -webkit-font-smoothing: antialiased;\n  font-smoothing: antialiased;\n  color: #435a6f;\n  font-size: 16px;\n  font-weight: 400;\n  line-height: 22px;\n  letter-spacing: -0.05px;\n"])));
-var templateObject_1;
-//# sourceMappingURL=font-styles.js.map
