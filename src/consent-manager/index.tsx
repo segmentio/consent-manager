0c3b34635301e6853c8f8f9dc5b268d5f5dff730
King Long Tse <kinglong.tse@segment.com>
2020-07-09 12:35:06 -0700

    bumped prettier version

--- src/consent-manager/index.tsx
+++ src/consent-manager/index.tsx
@@ -74,31 +74,35 @@ export default class ConsentManager extends PureComponent<ConsentManagerProps, {
           havePreferencesChanged,
           workspaceAddedNewDestinations
         }) => {
-          return <Container
-            customCategories={customCategories}
-            destinations={destinations}
-            newDestinations={newDestinations}
-            preferences={preferences}
-            isConsentRequired={isConsentRequired}
-            setPreferences={setPreferences}
-            resetPreferences={resetPreferences}
-            saveConsent={saveConsent}
-            closeBehavior={this.props.closeBehavior}
-            implyConsentOnInteraction={implyConsentOnInteraction ?? ConsentManager.defaultProps.implyConsentOnInteraction}
-            bannerContent={bannerContent}
-            bannerSubContent={bannerSubContent}
-            bannerTextColor={bannerTextColor || ConsentManager.defaultProps.bannerTextColor}
-            bannerBackgroundColor={
-              bannerBackgroundColor || ConsentManager.defaultProps.bannerBackgroundColor
-            }
-            preferencesDialogTitle={preferencesDialogTitle}
-            preferencesDialogContent={preferencesDialogContent}
-            cancelDialogTitle={cancelDialogTitle}
-            cancelDialogContent={cancelDialogContent}
-            havePreferencesChanged={havePreferencesChanged}
-            defaultDestinationBehavior={defaultDestinationBehavior}
-            workspaceAddedNewDestinations={workspaceAddedNewDestinations}
-          />
+          return (
+            <Container
+              customCategories={customCategories}
+              destinations={destinations}
+              newDestinations={newDestinations}
+              preferences={preferences}
+              isConsentRequired={isConsentRequired}
+              setPreferences={setPreferences}
+              resetPreferences={resetPreferences}
+              saveConsent={saveConsent}
+              closeBehavior={this.props.closeBehavior}
+              implyConsentOnInteraction={
+                implyConsentOnInteraction ?? ConsentManager.defaultProps.implyConsentOnInteraction
+              }
+              bannerContent={bannerContent}
+              bannerSubContent={bannerSubContent}
+              bannerTextColor={bannerTextColor || ConsentManager.defaultProps.bannerTextColor}
+              bannerBackgroundColor={
+                bannerBackgroundColor || ConsentManager.defaultProps.bannerBackgroundColor
+              }
+              preferencesDialogTitle={preferencesDialogTitle}
+              preferencesDialogContent={preferencesDialogContent}
+              cancelDialogTitle={cancelDialogTitle}
+              cancelDialogContent={cancelDialogContent}
+              havePreferencesChanged={havePreferencesChanged}
+              defaultDestinationBehavior={defaultDestinationBehavior}
+              workspaceAddedNewDestinations={workspaceAddedNewDestinations}
+            />
+          )
         }}
       </ConsentManagerBuilder>
     )
@@ -138,7 +142,7 @@ export default class ConsentManager extends PureComponent<ConsentManagerProps, {
       }
 
       destinations.forEach(destination => {
-        // Mark custom categories 
+        // Mark custom categories
         Object.entries(customCategories).forEach(([categoryName, { integrations }]) => {
           const consentAlreadySetToFalse = destinationPreferences[destination.id] === false
           const shouldSetConsent = integrations.includes(destination.id)
@@ -165,12 +169,18 @@ export default class ConsentManager extends PureComponent<ConsentManagerProps, {
 
     for (const destination of destinations) {
       // Mark advertising destinations
-      if (ADVERTISING_CATEGORIES.find(c => c === destination.category) && destinationPreferences[destination.id] !== false) {
+      if (
+        ADVERTISING_CATEGORIES.find(c => c === destination.category) &&
+        destinationPreferences[destination.id] !== false
+      ) {
         destinationPreferences[destination.id] = customPrefs.advertising
       }
 
       // Mark function destinations
-      if (FUNCTIONAL_CATEGORIES.find(c => c === destination.category) && destinationPreferences[destination.id] !== false) {
+      if (
+        FUNCTIONAL_CATEGORIES.find(c => c === destination.category) &&
+        destinationPreferences[destination.id] !== false
+      ) {
         destinationPreferences[destination.id] = customPrefs.functional
       }
 
