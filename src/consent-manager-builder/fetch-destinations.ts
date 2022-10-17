0bd5a5dc51e798595211c688239d3a793cface84
Edson Amaya <edson.amaya.dev@gmail.com>
2022-10-17 16:49:13 -0500

    Replace lodash-es by lodash to fix error with nextJs

--- src/consent-manager-builder/fetch-destinations.ts
+++ src/consent-manager-builder/fetch-destinations.ts
@@ -1,5 +1,5 @@
 import fetch from 'isomorphic-fetch'
-import { flatten, sortedUniqBy, sortBy } from 'lodash-es'
+import { flatten, sortedUniqBy, sortBy } from 'lodash'
 import { Destination } from '../types'
 
 async function fetchDestinationForWriteKey(
