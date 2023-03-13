acfa3cdcd59e56bb0f5ca915becda89852f12d22
Alex Saiannyi <67792608+bc-alexsaiannyi@users.noreply.github.com>
2023-03-13 17:05:02 +0100

    Revert "fix(storefront): BCTHEME-1368 remove tracking-preferences cookie (#17)" (#18)

--- src/consent-manager-builder/preferences.ts
+++ src/consent-manager-builder/preferences.ts
@@ -76,9 +76,4 @@ export function savePreferences({
     destinationPreferences,
     customPreferences
   })
-  // NOTE: bc-consent cookie is being used for the same purpose
-  cookies.remove(COOKIE_KEY, {
-    expires: COOKIE_EXPIRES,
-    domain
-  })
 }
